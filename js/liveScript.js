c9913dd5aaa608ea5038e49ae452ba305a82338d
Iruiasaro <brunettirosario@gmail.com>
2021-04-19 16:32:23 +0200

    applicazione coupon sconto

--- js/liveScript.js
+++ js/liveScript.js
@@ -1,31 +1,39 @@
-window.addEventListener("load",function(){
+window.addEventListener("load", function () {
 
     //inserimento variabili
-var ingredient = document.querySelectorAll(".ingredient [type='checkbox']");
-var ingredientPrice = document.getElementById("price");
-var button = document.getElementById ("button");
-var burgerName = document.getElementById ("name");
-var defaultPrice = 50 ;
+    var ingredient = document.querySelectorAll(".ingredient [type='checkbox']");
+    var ingredientPrice = document.getElementById("price");
+    var button = document.getElementById("button");
+    var burgerName = document.getElementById("name");
+    var defaultPrice = 50;
 
 
-button.addEventListener("click", function(){
-    //obbligo di inserire un nome
-  if (!burgerName.value){
-    alert("E' obbligatoria fornire un nome al Burger")
-  }else{
-    var prezzoAggiunta = 0;
-    for(var i = 0; i < ingredient.length; i++){
-      var ingredients = ingredient[i]
-      if(ingredients.checked){
-        prezzoAggiunta += parseInt(ingredients.value)
-      }
-    }
-    
-    var sommaTotale = defaultPrice + prezzoAggiunta
+    button.addEventListener("click", function () {
+        //obbligo di inserire un nome
+        if (!burgerName.value) {
+            alert("E' obbligatoria fornire un nome al Burger")
+        } else {
+            var prezzoAggiunta = 0;
+            for (var i = 0; i < ingredient.length; i++) {
+                var ingredients = ingredient[i]
+                if (ingredients.checked) {
+                    prezzoAggiunta += parseInt(ingredients.value)
+                }
+            }
 
-    //inserire coupon sconto
+            var sommaTotale = defaultPrice + prezzoAggiunta
 
+            //coupon per sconto
+            var coupon = document.getElementById("coupon")
+            var couponValid = ["burger2021", "burgerapril", "burgerdiscount"]
+            var discountCoupon = 0.15;
+            //verificare il coupon
+            if (couponValid.includes(coupon.value) === true) {
+                sommaTotale -= sommaTotale * discountCoupon;
+            }
+
+        }
+    })
 })
+    
 
-writePrice((sommaTotale), ingredientPrice)
-}
